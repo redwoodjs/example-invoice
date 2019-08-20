0a93729e9fa622d6a7b797e8eed5149307b1744c
Peter Pistorius <peter.pistorius@gmail.com>
2019-08-20 10:16:43 +0200

    Attempting to get zisi to deploy properly on Netlify.

--- api/scripts/package-functions.js
+++ api/scripts/package-functions.js
@@ -9,18 +9,18 @@ const FUNCTIONS_SRC_PATH = path.join(
   hammerConfig.baseDir,
   "api/dist/functions"
 );
-const FUNCTIONS_BUILD_PATH = path.join(
+const FUNCTIONS_DIST_PATH = path.join(
   hammerConfig.baseDir,
-  "api/dist/zippedFunctions/"
+  "api/dist-functions/"
 );
 
 try {
-  fs.mkdirSync(FUNCTIONS_BUILD_PATH);
+  fs.mkdirSync(FUNCTIONS_DIST_PATH);
 } catch (e) {
   console.log(e);
 }
 
-zipFunctions(FUNCTIONS_SRC_PATH, FUNCTIONS_BUILD_PATH)
+zipFunctions(FUNCTIONS_SRC_PATH, FUNCTIONS_DIST_PATH)
   .then(() => {
     console.log("All done.");
   })
