3a5d8624cbcf45dfd0cae98bfcb74a7139ec25ff
Peter Pistorius <peter.pistorius@gmail.com>
2019-09-04 10:00:59 +0200

    Make each preset-env specify a target.

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,5 @@
 module.exports = {
-  presets: [
-    "@babel/preset-react",
-    [
-      "@babel/preset-env",
-      // It is important to note that @babel/preset-env does not support stage-x plugins
-      {
-        targets: {
-          node: true
-        },
-        useBuiltIns: "usage",
-        corejs: 3
-      }
-    ],
-    "@babel/typescript"
-  ],
+  presets: ["@babel/preset-react", "@babel/typescript"],
   plugins: [
     ["@babel/plugin-proposal-class-properties", { loose: true }],
     ["@babel/plugin-proposal-export-default-from"],
