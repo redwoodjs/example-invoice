2288389fa87ee98db4550c6c6c98b843ccd32153
Peter Pistorius <peter.pistorius@gmail.com>
2019-09-08 17:09:24 +0200

    Duplicate prettier config rules in eslint config.

--- packages/eslint-config-hammer/index.js
+++ packages/eslint-config-hammer/index.js
@@ -1,3 +1,13 @@
+// Hammerframework's ESLint configuration is a mixture between ESLint's
+// recommended rules[1], React's recommended rules[2], and a bit of our stylistic
+// flair:
+// - no semicolons
+// - comma dangle when multiline
+// - single quotes
+// - always use parens around arrow functions
+//
+// 1. https://eslint.org/docs/rules/
+// 2. https://www.npmjs.com/package/eslint-plugin-react#list-of-supported-rules
 module.exports = {
   parser: 'babel-eslint',
   extends: [
@@ -34,13 +44,17 @@ module.exports = {
     camelcase: ['warn', { properties: 'never' }],
     'no-new': 'warn',
     'new-cap': ['error', { newIsCap: true, capIsNew: false }],
+    'space-before-function-paren': ['error', 'always'],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', 'always-multiline'],
     quotes: [
       'error',
       'single',
       { avoidEscape: true, allowTemplateLiterals: true },
     ],
-    'space-before-function-paren': ['error', 'always'],
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'arrow-parens': ['error', 'always'],
+    'object-curly-spacing': ['error', 'always'],
     // react rules
     'react/prop-types': [
       'error',
