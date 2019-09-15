f38cc159bd53010cc67ea47a66477cfa1a38d7fb
Peter Pistorius <peter.pistorius@gmail.com>
2019-09-15 20:55:21 +0200

    Flatten services/ db/ graphql things.

--- api/src/graphql/invoices.js
+++ api/src/graphql/invoices.js
@@ -1,8 +1,6 @@
 import { extendType, objectType, stringArg, intArg } from 'nexus'
 
-import { invoices } from 'src/services'
-
-export const Invoice = objectType({
+const Invoice = objectType({
   name: 'Invoice',
   definition (t) {
     t.int('id')
@@ -12,29 +10,87 @@ export const Invoice = objectType({
   },
 })
 
-export const extendMutation = extendType({
+export const invoiceCreate = extendType({
   type: 'Mutation',
   definition: (t) => {
-    t.field('invoicesCreate', {
+    t.field('invoiceCreate', {
       type: Invoice,
-      args: { id: intArg(), body: stringArg({ required: true }) },
-      async resolve (_root, { id, body }, { currentUser }) {
+      args: { body: stringArg({ required: true }) },
+      async resolve (_root, { body }, { currentUser, photon }) {
         const user = await currentUser()
-        return invoices.create({ id, user, body })
+        return await photon().invoices.create({
+          data: {
+            User: { connect: { id: user.id } },
+            body,
+          },
+        })
       },
     })
   },
 })
 
-export const extendQuery = extendType({
+export const invoiceUpdate = extendType({
+  type: 'Mutation',
+  definition: (t) => {
+    t.field('invoiceUpdate', {
+      type: Invoice,
+      args: {
+        id: intArg({ required: true }),
+        body: stringArg({ required: true }),
+      },
+      async resolve (_root, { id, body }, { currentUser, photon }) {
+        const user = await currentUser()
+        return await photon().invoices.update({
+          where: {
+            id,
+            User: user,
+          },
+          data: {
+            body,
+          },
+        })
+      },
+    })
+  },
+})
+
+// TODO: Pagination
+export const invoices = extendType({
   type: 'Query',
   definition: (t) => {
     t.list.field('invoices', {
       type: 'Invoice',
       nullable: true,
-      async resolve (_root, _args, { currentUser }) {
-        const user = await currentUser()
-        return invoices.newest({ user })
+      async resolve (_root, _args, { currentUser, photon }) {
+        return photon.invoices.findMany({
+          where: {
+            User: await currentUser(),
+          },
+          orderBy: {
+            createdAt: 'asc',
+          },
+        })
+      },
+    })
+  },
+})
+
+export const invoice = extendType({
+  type: 'Query',
+  definition: (t) => {
+    t.field('invoice', {
+      type: 'Invoice',
+      args: {
+        id: intArg({ required: true }),
+      },
+      nullable: true,
+      async resolve (_root, { id }, { currentUser, photon }) {
+        return photon.invoices.fineOne({
+          where: {
+            id,
+            User: await currentUser(),
+          },
+        })
       },
     })
   },
