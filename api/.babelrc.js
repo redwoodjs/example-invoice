1056c273af70093c879c1586d161f2e211191c26
Peter Pistorius <peter.pistorius@gmail.com>
2019-09-25 16:40:10 +0200

    Clean up configs.

--- api/.babelrc.js
+++ api/.babelrc.js
@@ -1,11 +1,5 @@
 const path = require('path')
 
-// // We need the absolute path of the src dir when building with
-// // `hammer-dev-server` but have to use the relative path when using
-// // `zip-it-and-ship-it`
-// const SRC_ALIAS__DIR =
-//   process.env.NODE_ENV === 'production' ? './src' : path.join(__dirname, 'src')
-
 module.exports = {
   extends: '../babel.config.js',
   presets: [
