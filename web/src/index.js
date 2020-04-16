2765b3eb862b0c0a326906a911d65bd7e2add61f
Peter Pistorius <peter.pistorius@gmail.com>
2020-04-16 10:46:14 +0200

    Try this out in client side.

--- web/src/index.js
+++ web/src/index.js
@@ -1,17 +1,22 @@
 import ReactDOM from 'react-dom'
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
+import { AuthProvider } from '@redwoodjs/auth'
+import createAuth0Client from '@auth0/auth0-spa-js'
 
 import theme from 'src/lib/theme'
 
 import Routes from './Routes'
 import './index.css'
 
+// create the auth client and pass it into the authprovider
+
 ReactDOM.render(
   <FatalErrorBoundary>
-    <RedwoodProvider theme={theme}>
-      <Routes />
-    </RedwoodProvider>
+    <AuthProvider type="auth0" client={createAuth0Client()}>
+      <RedwoodProvider theme={theme}>
+        <Routes />
+      </RedwoodProvider>
+    </AuthProvider>
   </FatalErrorBoundary>,
   document.getElementById('redwood-app')
 )
-
