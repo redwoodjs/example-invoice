8c1f2d6e7a61d2b31d9b4f6afbbcc94b6f378894
Peter Pistorius <peter.pistorius@gmail.com>
2020-05-10 15:38:25 +0200

    Add landing page.

--- web/src/pages/LandingPage/LandingPage.js
+++ web/src/pages/LandingPage/LandingPage.js
@@ -1,5 +1,95 @@
-import PageLayout from 'src/layouts/PageLayout'
+import { useAuth } from '@redwoodjs/auth'
+import { navigate, routes } from '@redwoodjs/router'
+
+import { Text, Flex, Box, Button } from 'src/lib/primitives'
+
+import exampleInvoice from './exampleInvoice.png'
 
 export default () => {
-  return <PageLayout>I am the marketing/ page</PageLayout>
+  const { loading, authenticated, login } = useAuth()
+
+  if (loading) {
+    return null
+  }
+
+  if (authenticated) {
+    navigate(routes.invoice())
+    return null
+  }
+
+  return (
+    <Box
+      bg="white"
+      css={`
+        overflow: hidden;
+      `}
+    >
+      <Flex>
+        <Box width={0.66} m="24px">
+          <Text
+            as="h1"
+            css={`
+              font-size: 90px;
+              font-weight: normal;
+              margin-bottom: 8px;
+            `}
+          >
+            Billable
+          </Text>
+          <Text
+            as="h2"
+            css={`
+              font-size: 48px;
+              font-weight: normal;
+              line-height: 1.3;
+              margin-bottom: 24px;
+            `}
+          >
+            Billing Made Simple. Period.
+          </Text>
+          <Text
+            as="h3"
+            css={`
+              font-size: 24px;
+              font-weight: normal;
+              margin-bottom: 24px;
+            `}
+          >
+            Billable is an editable template for invoicing your clients
+          </Text>
+
+          <Button
+            onClick={login}
+            css={`
+              height: 44px !important;
+              font-size: 20px !important;
+              background: blue !important;
+              padding: 0 16px !important;
+              color: white !important;
+            `}
+          >
+            Get Started
+          </Button>
+        </Box>
+        <Box
+          width={0.33}
+          bg="blue"
+          css={`
+            height: 100vh;
+            position: relative;
+          `}
+        >
+          <Box
+            as="img"
+            src={exampleInvoice}
+            css={`
+              position: absolute;
+              top: 48px;
+              left: -48px;
+            `}
+          />
+        </Box>
+      </Flex>
+    </Box>
+  )
 }
