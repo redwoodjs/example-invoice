521a0e3f110b70e5bea0f580c259273a5731960a
Peter Pistorius <peter.pistorius@gmail.com>
2020-07-18 16:09:22 +0200

    Add new landing page.

--- web/src/pages/LandingPage/LandingPage.js
+++ web/src/pages/LandingPage/LandingPage.js
@@ -1,10 +1,6 @@
 import { useAuth } from '@redwoodjs/auth'
 import { Redirect, routes } from '@redwoodjs/router'
 
-import { Text, Flex, Box, Button } from 'src/lib/primitives'
-
-import exampleInvoice from './exampleInvoice.png'
-
 export default () => {
   const { loading, isAuthenticated, logIn } = useAuth()
 
@@ -13,82 +9,56 @@ export default () => {
   }
 
   if (isAuthenticated) {
-    return <Redirect to={routes.invoice({ id: 'new' })} />
+    return <Redirect to={routes.invoices()} />
   }
 
   return (
-    <Box
-      bg="white"
-      css={`
-        overflow: hidden;
-      `}
-    >
-      <Flex>
-        <Box width={0.66} m="24px">
-          <Text
-            as="h1"
-            css={`
-              font-size: 90px;
-              font-weight: normal;
-              margin-bottom: 8px;
-            `}
-          >
-            Billable
-          </Text>
-          <Text
-            as="h2"
-            css={`
-              font-size: 48px;
-              font-weight: normal;
-              line-height: 1.3;
-              margin-bottom: 24px;
-            `}
-          >
-            Billing Made Simple. Period.
-          </Text>
-          <Text
-            as="h3"
-            css={`
-              font-size: 24px;
-              font-weight: normal;
-              margin-bottom: 24px;
-            `}
-          >
-            Billable is an editable template for invoicing your clients
-          </Text>
+    <div className="relative bg-gray-50 overflow-hidden h-screen">
+      <div className="relative pt-6 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
+        <div className="max-w-screen-xl mx-auto px-4 sm:px-6">
+          <nav className="relative flex items-center justify-between sm:h-10 md:justify-center">
+            <div className="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
+              <span className="inline-flex rounded-md shadow">
+                <a
+                  href="#"
+                  onClick={() => logIn()}
+                  className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo active:bg-gray-50 active:text-indigo-700 transition duration-150 ease-in-out"
+                >
+                  Log in
+                </a>
+              </span>
+            </div>
+          </nav>
+        </div>
 
-          <Button
-            onClick={logIn}
-            css={`
-              height: 44px !important;
-              font-size: 20px !important;
-              background: blue !important;
-              padding: 0 16px !important;
-              color: white !important;
-            `}
-          >
-            Get Started
-          </Button>
-        </Box>
-        <Box
-          width={0.33}
-          bg="blue"
-          css={`
-            height: 100vh;
-            position: relative;
-          `}
-        >
-          <Box
-            as="img"
-            src={exampleInvoice}
-            css={`
-              position: absolute;
-              top: 48px;
-              left: -48px;
-            `}
-          />
-        </Box>
-      </Flex>
-    </Box>
+        <main className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
+          <div className="text-center">
+            <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+              Example Invoice.
+              <br />
+              <span className="text-indigo-600">
+                Billing Made Simple.Period.
+              </span>
+            </h2>
+            <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+              Example Invoice is an open source invoice template built with{' '}
+              <a href="https://redwoodjs.com">Redwood,</a> the full stack
+              framework for the Jamstack.
+            </p>
+            <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+              <div className="rounded-md shadow">
+                <a
+                  href="#"
+                  onClick={() => logIn()}
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
+                >
+                  Get started
+                </a>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
   )
 }
