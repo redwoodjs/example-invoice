6581957d96b814470133e3246ff8e47ba8864f99
Dale French <dalef@fmi.co.za>
2020-10-12 19:45:19 +0200

    fix: graphql handler

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -3,20 +3,20 @@ import {
   makeMergedSchema,
   makeServices,
 } from '@redwoodjs/api'
-import { db } from 'src/lib/db'
 
 import services from 'src/services/**/*.{ts,js}'
 import schemas from 'src/graphql/**/*.{ts,js}'
+import { db } from 'src/lib/db'
 
 import { getCurrentUser } from 'src/lib/auth'
 
 export const handler = createGraphQLHandler({
-  getCurrentUser,
   schema: makeMergedSchema({
     schemas,
     services: makeServices({ services }),
   }),
+  getCurrentUser,
   onException: () => {
-    db.disconnect()
+    db.$disconnect()
   },
 })
